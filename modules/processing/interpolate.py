043bd85e4e23c22ea74c57f325342eaa5b00560f
JulianSoZa <juliansoza19@gmail.com>
2025-12-14 15:47:14 -0500

    📡 Update interpolate.py
    Update interpolate_data function: return shape

--- modules/processing/interpolate.py
+++ modules/processing/interpolate.py
@@ -3,8 +3,11 @@ import alphashape
 from shapely.geometry import Point
 import numpy as np
 
-def interpolate_data(df_sample, var, coords_type = ('x', 'y'), nx=500, ny=500, alpha=0.0001):
-    grid_x, grid_y = np.meshgrid(np.linspace(df_sample[coords_type[0]].values.min(), df_sample[coords_type[0]].values.max(), nx), np.linspace(df_sample[coords_type[1]].values.min(), df_sample[coords_type[1]].values.max(), ny))
+def interpolate_data(df_sample, var, coords_type = ('x', 'y'), nx=500, ny=500, alpha=0.0001, return_shape=False, x_values=None, y_values=None, **kwargs):
+    if x_values is not None and y_values is not None:
+        grid_x, grid_y = np.meshgrid(x_values, y_values)
+    else:
+        grid_x, grid_y = np.meshgrid(np.linspace(df_sample[coords_type[0]].values.min(), df_sample[coords_type[0]].values.max(), nx), np.linspace(df_sample[coords_type[1]].values.min(), df_sample[coords_type[1]].values.max(), ny))
     data_interpolated = griddata(np.column_stack((df_sample[coords_type[0]].values, df_sample[coords_type[1]].values)), df_sample[var].values, (grid_x.ravel(), grid_y.ravel()), method='cubic')
 
     shape = alphashape.alphashape(np.column_stack((df_sample[coords_type[0]].values, df_sample[coords_type[1]].values)), alpha=alpha)
@@ -14,4 +17,7 @@ def interpolate_data(df_sample, var, coords_type = ('x', 'y'), nx=500, ny=500, a
     inside_mask = np.array([shape.contains(Point(p)) for p in points])
 
     data_masked = np.where(inside_mask, data_interpolated, np.nan)
-    return grid_x, grid_y, data_interpolated, data_masked, inside_mask
+    if return_shape:
+        return grid_x, grid_y, data_interpolated, data_masked, inside_mask, shape
+    else:
+        return grid_x, grid_y, data_interpolated, data_masked, inside_mask
